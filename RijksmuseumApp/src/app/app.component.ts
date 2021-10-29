8dba0346965f6144bf1e639b1ee2893da3fc2f12
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-10-29 19:04:40 +0200

    progress fetching data
    user inputs the search string and the app  displays the result with images and data about each object

--- RijksmuseumApp/src/app/app.component.ts
+++ RijksmuseumApp/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
   title = 'RijksmuseumApp';
+  artObjects: any;
+  userSearch = "poop"
+  constructor(private service: ApiService) {
+    this.fetchUserSearch();
+  }
+  update(value: string) {
+    this.userSearch = value
+    this.fetchUserSearch();
+  }
+  fetchUserSearch() {
+    this.service.fetchCollection(this.userSearch).subscribe((data) => {
+      console.log(data)
+      this.artObjects = data.artObjects
+    })
+  }
+
 }
