a30eaba7a1e0756136b7469dbb524c7361a7baac
Oxana Bulanova <oxana_bulanova@hotmail.com>
2021-10-30 18:08:24 +0200

    data about object on new page
    introduced user driven search. Search objects are clickable and return more information about object and full sized image

--- RijksmuseumApp/src/app/app.module.ts
+++ RijksmuseumApp/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './api.module';
+import { AppRoutingModule } from './routes.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OverviewComponent } from './overview/overview.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
